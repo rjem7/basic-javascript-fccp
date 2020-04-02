25b6fb461e6479bf9287fe1bfb4f1ec34d204be0
rjem7 <rkrajeshin@gmail.com>
2020-04-01 20:49:40 -0400

    Remaining code from fccp

---
+++ src/manipulating-complex-objects.js
@@ -0,0 +1,24 @@
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  // Add a record here
+  {
+    "artist": "Billy Joel 2",
+    "title": "Piano Man 2",
+    "release_year": 1937,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ]
+  }
+];
